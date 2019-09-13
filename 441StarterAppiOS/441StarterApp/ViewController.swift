28dfe4cd73b28c9bc0f56bfe09fa042a5e8f66b6
Alecg956 <alechg@umich.edu>
2019-09-13 13:03:39 -0400

    Created iOS starter app project file

---
+++ 441StarterAppiOS/441StarterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  441StarterApp
+//
+//  Created by Alec Goldberg on 9/13/19.
+//  Copyright © 2019 Alec Goldberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
